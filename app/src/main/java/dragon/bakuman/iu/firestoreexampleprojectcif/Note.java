250fce636e6880c01a295f680519d9d83fabccfa
iudragon <42891947+TeamLewok@users.noreply.github.com>
2018-12-25 14:18:16 +0530

    8 lec CIF. Add and retrieve multiple docs

--- app/src/main/java/dragon/bakuman/iu/firestoreexampleprojectcif/Note.java
+++ app/src/main/java/dragon/bakuman/iu/firestoreexampleprojectcif/Note.java
@@ -1,11 +1,23 @@
 package dragon.bakuman.iu.firestoreexampleprojectcif;
 
+import com.google.firebase.firestore.Exclude;
+
 public class Note {
+    private String documentId;
     private String title;
     private String description;
 
     public Note() {
         //public no-arg constructor needed
+    }
+
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
     }
 
     public Note(String title, String description) {
