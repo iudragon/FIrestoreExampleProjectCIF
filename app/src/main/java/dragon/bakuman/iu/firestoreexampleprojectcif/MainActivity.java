bff17e1dc0041eba0379df14a82634f8906e6389
iudragon <42891947+TeamLewok@users.noreply.github.com>
2018-12-25 12:36:14 +0530

    6 Lec Firestore. Updating Description only

--- app/src/main/java/dragon/bakuman/iu/firestoreexampleprojectcif/MainActivity.java
+++ app/src/main/java/dragon/bakuman/iu/firestoreexampleprojectcif/MainActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -165,6 +166,20 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    public void updateDescription(View view) {
+
+        //input from our EditText field
+        String description = mEditTextDescription.getText().toString();
+
+       /* Map<String, Object> note = new HashMap<>();
+        note.put(KEY_DESCRIPTION, description);
+
+        noteRef.set(note, SetOptions.merge());*/
+       noteRef.update(KEY_DESCRIPTION, description);
+
 
     }
 }
